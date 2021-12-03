a03ee80dfd8c4c4160caad5d74efd0f498998905
Rodrigo Calmon <rodrigo.ccalmon@gmail.com>
2021-12-03 15:14:39 -0300

    1

---
+++ app.js
@@ -0,0 +1,8 @@
+//esse arquivo vai 
+
+const app = require('./src/config/server');
+const porta = process.env.PORTA;
+
+app.listen(porta, () => console.log('Api rodando na porta ' + porta))
+
+
---
+++ src/config/server.js
@@ -0,0 +1,30 @@
+require('dotenv').config();
+/*
+require é como se fosse o import.
+require dotenv config ele vai importar tudo que tem no env e vai disponibilar para usar
+*/
+
+const express = require('express');
+//para trab com req http.
+
+const cors = require('cors');
+
+// const routes = require();
+
+const app = express();
+//app vai ser o express como se fosse uma função
+//td q tenho dentro do express vou poder acessar com app. 
+
+app.use(cors()); 
+//serve para qnd fizer requisição na api a api nao bloequeie a requisiçao no meio do caminhno. esse app.use(cors()); deixo qlqr um usar minha api
+
+app.use(express.json({limit: '50mb'}))
+
+//app.use(routes);
+
+
+module.exports = app;
+
+
+
+
