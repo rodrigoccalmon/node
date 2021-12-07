d19694eb710335790351f3f64d42ad29c11fdcf0
Rodrigo Calmon <rodrigo.ccalmon@gmail.com>
2021-12-07 11:24:16 -0300

    updt index

--- src/database/index.js
+++ src/database/index.js
@@ -2,4 +2,7 @@ const Sequelize = require('sequelize');
 const dbConfig = require('../config/database');
 const connection = new Sequelize(dbConfig);
 
+import Perfil from '../models/Perfil';
 
+Perfil.init(connection);
+module.exports = connection;
--- src/database/migrations/20211207135550-criar-tabela-perfis.js
+++ src/database/migrations/20211207135550-criar-tabela-perfis.js
@@ -25,11 +25,6 @@ module.exports = {
   },
 
   down: async (queryInterface, Sequelize) => {
-    /**
-     * Add reverting commands here.
-     *
-     * Example:
-     * await queryInterface.dropTable('users');
-     */
+    return queryInterface.dropTable('perfis')
   },
 };
