b7de84eb4c1f696d6322f00b733eaf8d04c6c488
Rodrigo Calmon <rodrigo.ccalmon@gmail.com>
2021-12-09 11:37:42 -0300

    dto

---
+++ src/dtos/UsuarioDTO.js
@@ -0,0 +1,17 @@
+//Classe que vai ter toda lógica de como funciona o usuário dentro da aplicação
+
+const PerfilDTO = require("./PerfilDTO");
+
+module.exports = class UsuarioDTO {
+    constructor(obj) {
+        obj = obj || {};
+        this.id = obj.id;
+        this.nome = obj.nome;
+        this.email = obj.email;
+        this.senha = obj.senha;
+        this.perfil = obj.perfil && new PerfilDTO(obj.perfil);
+        this.dataInativacao = obj.dataInativacao;
+        this.criadoEm = obj.criadoEm;
+        this.atualizadoEm = obj.atualizadoEm;
+    }
+}
